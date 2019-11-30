07eeeefe3d08bdc6151b89c99cb4d1b1876809cb
RogerNoel <noel.roger.pro@gmail.com>
2019-11-30 21:34:07 +0100

    impl. existing email verif. in post/api/users

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,11 @@ router.post('/', async (req, res)=>{
     try {
         const { error } = validateUser(req.body)
         if (error) return res.status(400).send(error.details[0].message)
-        const user = new User({
+
+        let user = await User.findOne({email: req.body.email});
+        if (user) return res.status(400).send('This email is already used.')
+
+        user = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
